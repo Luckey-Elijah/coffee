3a9f8043adbaee11331ba545e2bedcecf186d728
Elijah Luckey <luckeyelijah112@gmail.com>
2021-04-13 10:13:41 -0400

    Adds grinder class and appropiate enums

---
+++ lib/src/classes/grinder.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+part '../enums/grinder_type.dart';
+part 'grinder_range.dart';
+
+/// {@template grinder}
+/// The tool used for grinding coffee beans.
+/// {@endtemplate}
+class Grinder extends Equatable {
+  /// {@macro grinder}
+  Grinder({
+    required this.make,
+    required this.model,
+    this.stepped,
+    this.type,
+    this.range,
+  });
+
+  /// The "brand" or company that makes this grinder.
+  final String make;
+
+  /// The name of the grinder.
+  final String model;
+
+  /// A stepped grinder refers to how the adjustments of grind sizes are made.
+  /// Typically, only burr grinders have this option.
+  final bool? stepped;
+
+  /// The grinder type: a burr or blade grinder.
+  final GrinderType? type;
+
+  /// The range of the grinder adjustment size.
+  final GrinderRange? range;
+
+  @override
+  List<Object?> get props => [
+        make,
+        model,
+        stepped,
+        type,
+        range,
+      ];
+}
---
+++ lib/src/classes/grinder_range.dart
@@ -0,0 +1,20 @@
+part of 'grinder.dart';
+
+/// {@template grinder_range}
+/// The range of grind size for your grinder.
+/// {@endtemplate}
+class GrinderRange extends Equatable {
+  /// {@macro grinder_range}
+  GrinderRange(this.lower, this.upper)
+      : assert(lower < upper),
+        assert(lower > 0 && upper > 0);
+
+  /// The lower range of the grinder.
+  final num lower;
+
+  /// The upper range of the grinder.
+  final num upper;
+
+  @override
+  List<Object?> get props => [lower, upper];
+}
