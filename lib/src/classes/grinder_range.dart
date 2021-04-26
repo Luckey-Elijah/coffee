65da14302b3658cc967e08720fb20b6dec876ff8
Elijah Luckey <luckeyelijah112@gmail.com>
2021-04-26 17:16:57 -0400

    Removes unreach assert statement

--- lib/src/classes/grinder_range.dart
+++ lib/src/classes/grinder_range.dart
@@ -7,8 +7,7 @@ class GrinderRange extends Equatable {
   /// {@macro grinder_range}
   const GrinderRange(this.lower, this.upper)
       : assert(lower < upper),
-        assert(!(lower < -0.0)),
-        assert(!(upper < -0.0));
+        assert(!(lower < -0.0));
 
   /// The lower range of the grinder.
   final num lower;
